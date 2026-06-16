1bfdc031852f8af8c1f00c4c6600f159e0c578d3
plusdojo2026-Tomikawa <tomikawa-haruto-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-16 10:56:08 +0900

    Update d3.sql

--- source/SQLバックアップ/d3.sql
+++ source/SQLバックアップ/d3.sql
@@ -18,9 +18,9 @@
 --
 -- Table structure for table `coffee`
 --
-CREATE DATABASE d3;
-USE d3;
-					
+DROP TABLEの上にCREATE DATABASE webapp1;
+USE webapp1
+
 DROP TABLE IF EXISTS `coffee`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
@@ -29,7 +29,6 @@ CREATE TABLE `coffee` (
   `name` varchar(50) DEFAULT NULL,
   `valu` int DEFAULT NULL,
   `newvalu` int DEFAULT NULL,
-  `myvalu` int DEFAULT NULL,
   `size` int DEFAULT NULL,
   `bland` varchar(50) DEFAULT NULL,
   `area` varchar(50) DEFAULT NULL,
@@ -40,7 +39,7 @@ CREATE TABLE `coffee` (
   `picture` varchar(200) DEFAULT NULL,
   `memo` varchar(200) DEFAULT NULL,
   PRIMARY KEY (`number`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -49,6 +48,7 @@ CREATE TABLE `coffee` (
 
 LOCK TABLES `coffee` WRITE;
 /*!40000 ALTER TABLE `coffee` DISABLE KEYS */;
+INSERT INTO `coffee` VALUES (1,'エスプレッソ',300,280,30,'ダークロースト','イタリア','個人店','Caffe Roma',3,1,'espresso.jpg','濃厚で苦味が強い'),(2,'アメリカーノ',350,330,240,'ミディアムロースト','アメリカ','チェーン','Star Coffee',2,0,'americano.jpg','すっきりした味'),(3,'カフェラテ',450,420,300,'ミディアムロースト','フランス','チェーン','Cafe Paris',1,1,'latte.jpg','ミルク多めで飲みやすい'),(4,'カプチーノ',400,380,180,'ミディアムロースト','イタリア','カフェ','Roma Cafe',2,0,'cappuccino.jpg','泡が特徴的'),(5,'モカ',500,470,300,'ダークロースト','エチオピア','専門店','Mocha House',2,1,'mocha.jpg','チョコ風味'),(6,'コールドブリュー',420,400,350,'ライトロースト','日本','チェーン','Tokyo Brew',1,0,'coldbrew.jpg','苦味控えめで爽やか'),(7,'フラットホワイト',480,450,200,'ミディアムロースト','オーストラリア','カフェ','Sydney Beans',2,1,'flatwhite.jpg','きめ細かいミルク'),(8,'アイスコーヒー',320,300,350,'ミディアムロースト','日本','チェーン','Cool Coffee',1,0,'icedcoffee.jpg','夏に人気'),(9,'デカフェ',380,360,250,'ライトロースト','コロンビア','専門店','Green Beans',1,0,'decaf.jpg','カフェインレス'),(10,'ウインナーコーヒー',550,520,220,'ダークロースト','オーストリア','喫茶店','Vienna Cafe',3,1,'vienna.jpg','クリームが乗った甘いコーヒー');
 /*!40000 ALTER TABLE `coffee` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -65,7 +65,7 @@ CREATE TABLE `food` (
   `foodname` varchar(50) DEFAULT NULL,
   `foodmemo` varchar(200) DEFAULT NULL,
   PRIMARY KEY (`foodnumber`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -74,6 +74,7 @@ CREATE TABLE `food` (
 
 LOCK TABLES `food` WRITE;
 /*!40000 ALTER TABLE `food` DISABLE KEYS */;
+INSERT INTO `food` VALUES (1,1,'ドーナツ','めちゃめちゃ甘くておいしい');
 /*!40000 ALTER TABLE `food` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -86,7 +87,7 @@ DROP TABLE IF EXISTS `history`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `history` (
   `old_number` int NOT NULL AUTO_INCREMENT,
-  `history_number` int DEFAULT NULL,
+  `coffee_number` int DEFAULT NULL,
   PRIMARY KEY (`old_number`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -114,7 +115,7 @@ CREATE TABLE `idpw` (
   `password` varchar(32) DEFAULT NULL,
   `mylevel` int DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -123,6 +124,7 @@ CREATE TABLE `idpw` (
 
 LOCK TABLES `idpw` WRITE;
 /*!40000 ALTER TABLE `idpw` DISABLE KEYS */;
+INSERT INTO `idpw` VALUES (1,'plusdojo@gmail.com','id','password',1);
 /*!40000 ALTER TABLE `idpw` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -135,4 +137,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-06-12 11:24:45
+-- Dump completed on 2026-06-16 10:51:41
