464c04ace98cce610118d00772f24b3200052efa
plusdojo2026-Tomikawa <tomikawa-haruto-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-18 14:10:06 +0900

    Update IdPwDAO.java

--- source/src/main/java/dao/IdPwDAO.java
+++ source/src/main/java/dao/IdPwDAO.java
@@ -69,12 +69,71 @@ public class IdPwDAO {
 
 		// 結果を返す
 		return UserList;
-		//ここにインサート分
 	}
 	
-	//ここまで登録プログラム
-	//ログイン後に対象のレコードを取り出すメソッド、検索条件にemailとパスワードを使う。
-	
-}
+	//ここから登録のプログラム
+		public boolean insert(IdPw UserList) {
+			Connection conn = null;
+			boolean result = false;
 
+			try {
+				// JDBCドライバを読み込む
+				Class.forName("com.mysql.cj.jdbc.Driver");
+
+				// データベースに接続する
+				conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/d3?"
+						+ "characterEncoding=utf8&useSSL=false&serverTimezone=GMT%2B9&rewriteBatchedStatements=true",
+						"root", "password");
+
+				// SQL文を準備する
+				String sql = "INSERT INTO IdPw VALUES (0, ?, ?, ?, ?)";
+				PreparedStatement pStmt = conn.prepareStatement(sql);
+
+				// SQL文を完成させる
+				if (UserList.getEmail() != null) {
+					pStmt.setString(1, UserList.getEmail());
+				} else {
+					pStmt.setString(1, "");
+				}
+				if (UserList.getUser() != null) {
+					pStmt.setString(2, UserList.getUser());
+				} else {
+					pStmt.setString(2, "");
+				}
+				if (UserList.getPassword() != null) {
+					pStmt.setString(3, UserList.getPassword());
+				} else {
+					pStmt.setString(3, "");
+				}
+				pStmt.setInt(4, UserList.getMylevel());
+			
+
+				// SQL文を実行する
+				if (pStmt.executeUpdate() == 1) {
+					result = true;
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			} finally {
+				// データベースを切断
+				if (conn != null) {
+					try {
+						conn.close();
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+
+			// 結果を返す
+			return result;
+		}
+		//ここまで登録プログラム
+
+}
+	
+				
+		
 
